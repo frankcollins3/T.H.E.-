52501d178aa1bd86abd1eb91969e441718a5c50a
Frank Collins <fwc3rd@gmail.com>
2024-06-04 10:54:07 -0400

    given issue #1, interim <Context.Provider> served JSON [10:53am]
    also considered making additionally complex by:
    stringifiedJSON() in AWS S3, serving to client JSON.parse()

---
+++ interfaceTypes.ts
@@ -0,0 +1,12 @@
+export interface keyRatiosINTERFACE {
+    id: number,
+    marketCap: number,
+    sharesOutstanding: number,
+    peRatio: number,
+    psRatio: number,
+    pbRatio: number,
+    pegRatio: number,
+    currentRatio: number,
+    debtToEquityRatio: number,
+    EPS: number,
+}
