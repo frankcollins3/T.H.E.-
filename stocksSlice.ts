0b0e6786ebf41c5ecbf57dde2dc314e17f57b94c
Frank Collins <fwc3rd@gmail.com>
2024-06-04 20:42:24 -0400

    complete(ish) [8:42pm]

--- stocksSlice.ts
+++ stocksSlice.ts
@@ -3,7 +3,13 @@ import { APPLEcompanyINTERFACE } from 'Interface/InterfaceTypes';
 
 interface StocksSliceState {
   APPLE: APPLEcompanyINTERFACE;
-  CANDLESTICK_CHART_VIEW_MULTI: false;
+
+  // toggles, determines to which of the below state options a user's selection applies: CANDLESTICK_CHART_SINGLE_SELECTION | CANDLESTICK__MULTI_SELECTION
+  CANDLESTICK_CHART_VIEW_MULTI: boolean;
+
+  // selection being: OHLCV.      multi selection splits between showing as 2 lines on linechart:   Open,Close | High,Low
+  CANDLESTICK_CHART_SINGLE_SELECTION: string;   // 'o, h, l, c, v' candlestick
+  CANDLESTICK_CHART_MULTI_SELECTION: string; // 'oc', 'hl',  || 'all'
 
 }
 
@@ -38,8 +44,10 @@ const initialState: StocksSliceState = {
         }   
     },
 
-    // 
-  CANDLESTICK_CHART_VIEW_MULTI: false
+    // CANDLESTICK_CHART_VIEW_MULTI
+  CANDLESTICK_CHART_VIEW_MULTI: false,
+  CANDLESTICK_CHART_SINGLE_SELECTION: '',
+  CANDLESTICK_CHART_MULTI_SELECTION: '',
 }
                                         
 const stocksSlice = createSlice({
@@ -48,17 +56,26 @@ const stocksSlice = createSlice({
   reducers: { 
 
     SET_APPLE: (state, action) => { state.APPLE = action.payload },
-
+    
     SET_APPLE_KEY_RATIOS: (state, action) => { state.APPLE.keyRatios = action.payload },    
     SET_APPLE_CANDLESTICK: (state, action) => { state.APPLE.candleStick = action.payload },    
     SET_APPLE_TICKER: (state, action) => { state.APPLE.tradeTicker = action.payload },    
- 
+
+    TOGGLE_CANDLESTICK_CHART_VIEW_MULTI: (state) => { state.CANDLESTICK_CHART_VIEW_MULTI = !state.CANDLESTICK_CHART_VIEW_MULTI },    
+    
+    SET_CANDLESTICK_CHART_SINGLE_SELECTION: (state, action) => { state.CANDLESTICK_CHART_SINGLE_SELECTION = action.payload },
+    SET_CANDLESTICK_CHART_MULTI_SELECTION: (state, action) => { state.CANDLESTICK_CHART_MULTI_SELECTION = action.payload },
+    // when multi view is selected single view will reset and vice versa. 
+
   },
 });
 
 export const 
 { 
-       
+    SET_APPLE, SET_APPLE_KEY_RATIOS, SET_APPLE_CANDLESTICK, SET_APPLE_TICKER,        
+    TOGGLE_CANDLESTICK_CHART_VIEW_MULTI, 
+    SET_CANDLESTICK_CHART_SINGLE_SELECTION, SET_CANDLESTICK_CHART_MULTI_SELECTION
+
 } = stocksSlice.actions;
 
 export default stocksSlice.reducer;
