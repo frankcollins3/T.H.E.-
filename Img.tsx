58581034df366b73e3e5adfe953c84e5acc02696
Frank Collins <fwc3rd@gmail.com>
2024-06-07 23:45:37 -0400

    company logo images [11:45pm]

--- Img.tsx
+++ Img.tsx
@@ -10,6 +10,15 @@ type imgContextType = {
     appleLogo: string;
     keyIcon: string;
     commonwealth: string;
+
+    bofaLogo: string;
+    citibankLogo: string;
+    goldmanSachsLogo: string;
+    morganStanleyLogo: string;
+    jpMorganLogo: string;
+    moelisLogo: string;
+    lazardLogo: string;
+    evercoreLogo: string;
 }
 
 // define values which will remain static
@@ -22,6 +31,15 @@ const imgDefaults: imgContextType = {
     appleLogo: 'img/appleLogo.png',
     keyIcon: 'img/key.png',
     commonwealth: 'img/commonwealth.png',
+
+    bofaLogo: 'img/bofaLogo.png',
+    citibankLogo: 'img/citibankLogo.png',
+    goldmanSachsLogo: 'img/goldmanSachsLogo.png',
+    morganStanleyLogo: 'img/morganStanleyLogo.png',
+    jpMorganLogo: 'img/jpMorganLogo.png',
+    moelisLogo: 'img/moelisLogo.png',
+    lazardLogo: 'img/lazardLogo.png',
+    evercoreLogo: 'img/evercoreLogo.png',
 };
 
 // createContext
@@ -44,7 +62,16 @@ export function ImgProvider({ children }: Props) {
     const [ADazure, setAdazure] = useState<string>(`img/ADazure.png`);      
     const [appleLogo, setappleLogo] = useState<string>(`img/appleLogo.png`);      
     const [keyIcon, setkeyIcon] = useState<string>(`img/key.png`);      
-    const [commonwealth, setcommonwealth] = useState<string>(`img/commonwealth.png`);      
+    const [commonwealth, setcommonwealth] = useState<string>(`img/commonwealth.png`);  
+
+    const [bofaLogo, setbofaLogo] = useState<string>(`img/bofaLogo.png`);      
+    const [citibankLogo, setcitibankLogo] = useState<string>(`img/citibankLogo.png`);      
+    const [goldmanSachsLogo, setgoldmanSachsLogo] = useState<string>(`img/goldmanSachsLogo.png`);      
+    const [morganStanleyLogo, setmorganStanleyLogo] = useState<string>(`img/morganStanleyLogo.png`);      
+    const [jpMorganLogo, setjpMorganLogo] = useState<string>(`img/jpMorganLogo.png`);      
+    const [moelisLogo, setMoelisLogo] = useState<string>(`img/moelisLogo.png`);      
+    const [lazardLogo, setlazardLogo] = useState<string>(`img/lazardLogo.png`);      
+    const [evercoreLogo, setevercoreLogo] = useState<string>(`img/evercoreLogo.png`);      
 
     const value = {
         singleCheckbox,
@@ -54,8 +81,18 @@ export function ImgProvider({ children }: Props) {
         ADazure,
         appleLogo,
         keyIcon,
-        commonwealth
+        commonwealth,
+
+        bofaLogo,
+        citibankLogo,
+        goldmanSachsLogo,
+        morganStanleyLogo,
+        jpMorganLogo,
+        moelisLogo,
+        lazardLogo,
+        evercoreLogo        
     };
+    // bofa, moelis, citibank, goldman sachs, morgan stanley, jpmorgan, lazard, evercore, moelis, 
 
     return (
         <>
